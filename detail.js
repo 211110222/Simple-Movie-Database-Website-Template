9ad37a5deff3464bf4f700327d8066a72af320cf
Abhishek Singh <124887245+BeingLazyCoder@users.noreply.github.com>
2024-10-05 22:22:50 +0530

    Add files via upload

--- detail.js
+++ detail.js
@@ -1,16 +1,16 @@
 const apiKey = '73da3c2a52ca1beaa0d9360994a2d8a2';
 const urlParams = new URLSearchParams(window.location.search);
 const movieId = urlParams.get('id');
-const mediaType = 'movie'
+const mediaType = urlParams.get('type');
 
 // Fetch movie details
-function fetchMovieDetails(movieId) {
-    const url = `https://api.themoviedb.org/3/${movieId}?api_key=${apiKey}&append_to_response=videos`;
+function fetchMovieDetails(movieId, mediaType) {
+    const url = `https://api.themoviedb.org/3/${mediaType}/${movieId}?api_key=${apiKey}&append_to_response=videos`;
     fetch(url)
         .then(response => response.json())
         .then(data => {
             displayMovieDetails(data);
-            fetchRelatedMovies(movieId); // Fetch related movies after movie details
+            fetchRelatedMovies(movieId, mediaType); // Fetch related movies after movie details
         })
         .catch(error => {
             console.error('Error fetching movie details:', error);
@@ -33,8 +33,8 @@ function displayMovieDetails(movie) {
 }
 
 // Fetch related movies
-function fetchRelatedMovies(movieId) {
-    const url = `https://api.themoviedb.org/3/${movieId}/similar?api_key=${apiKey}`;
+function fetchRelatedMovies(movieId, mediaType) {
+    const url = `https://api.themoviedb.org/3/${mediaType}/${movieId}/similar?api_key=${apiKey}`;
     fetch(url)
         .then(response => response.json())
         .then(data => {
@@ -68,5 +68,5 @@ function showDetails(id, type) {
 
 // Fetch movie details on page load
 document.addEventListener('DOMContentLoaded', function () {
-    fetchMovieDetails(movieId);
+    fetchMovieDetails(movieId, mediaType);
 });
